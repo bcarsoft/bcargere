02c01dad5a5801249aa4a9f1cf873a2feb35a831
abelbcarvalho <abelbdecarvalho@gmail.com>
2021-02-17 15:40:42 -0300

    mensagem de erro em cartao

--- com/bcargere/cartao/service/service_cartao.py
+++ com/bcargere/cartao/service/service_cartao.py
@@ -12,6 +12,7 @@ class ServiceCartao(IServiceCartao):
     """
 
     def __init__(self):
+        """Novo service cartao."""
         self._card = CartaoCheck()
 
     # cria cartão
@@ -23,8 +24,10 @@ class ServiceCartao(IServiceCartao):
         :return: bool
         """
         if not Instan.get_instance(cartao, Cartao):
+            SingMessage.message().message = 'Error: Instancia Inválida.'
             return False
         elif not cartao.fk > 0:
+            SingMessage.message().message = 'Error: ID Banco Inválido.'
             return False
         return False if not self._check_create_update(cartao) else True
 
@@ -37,10 +40,13 @@ class ServiceCartao(IServiceCartao):
         :return: bool
         """
         if not Instan.get_instance(cartao, Cartao):
+            SingMessage.message().message = 'Error: Instancia Inválida.'
             return False
         elif not cartao.id > 0:
+            SingMessage.message().message = 'Error: ID Cartão Inválido.'
             return False
         elif not cartao.fk > 0:
+            SingMessage.message().message = 'Error: ID Banco Inválido.'
             return False
         return False if not self._check_create_update(cartao) else True
 
@@ -53,7 +59,10 @@ class ServiceCartao(IServiceCartao):
         :return: bool
         """
         if not Instan.get_instance(cartao, Cartao):
+            SingMessage.message().message = 'Error: Instancia Inválida.'
             return False
+        if not cartao.id > 0:
+            SingMessage.message().message = 'Error: ID Cartão Inválido.'
         return False if not cartao.id > 0 else True
 
     # busca cartao
@@ -65,6 +74,7 @@ class ServiceCartao(IServiceCartao):
         :return: int
         """
         if not kwargs or not kwargs.__len__() > 0:
+            SingMessage.message().message = 'Error: Pesquisa Inválida.'
             return 0
         else:
             return 1
@@ -78,22 +88,31 @@ class ServiceCartao(IServiceCartao):
         :return: bool
         """
         if not self._card_.bandeira_checker(cartao.bandeira):
+            SingMessage.message().message = 'Error: Bandeira Inválida.'
             return False
         elif not self._card_.tipo_check(cartao.tipo):
+            SingMessage.message().message = 'Error: Tipo Inválido.'
             return False
         elif not self._card_.nome_checker(cartao.nome_cartao):
+            SingMessage.message().message = 'Error: Nome Inválido.'
             return False
         elif not self._card_.str_to_number_cartao(cartao.numero):
+            SingMessage.message().message = 'Error: Número Inválido.'
             return False
         elif not self._card_.data_checker(cartao.data_venc):
+            SingMessage.message().message = 'Error: Data de Vencimento Inválida.'
             return False
         elif not self._card_.code_checker(cartao.codigo):
+            SingMessage.message().message = 'Error: Código Inválido.'
             return False
         elif not self._card_.verifica_monetario(cartao.limite):
+            SingMessage.message().message = 'Error: Limite Inválido.'
             return False
         elif not self._card_.verifica_monetario(cartao.excedente):
+            SingMessage.message().message = 'Error: Excedente Inválido.'
             return False
         elif not self._card_.verifica_monetario(cartao.fatura):
+            SingMessage.message().message = 'Error: Fatura Inválida.'
             return False
         else:
             return True
